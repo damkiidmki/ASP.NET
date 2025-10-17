c58d74860b9b4cc4216477f5fdf57d6557a0492e
Dima <damkiidmki@yandex.ru>
2025-10-18 02:40:33 +0330

    Добавлены тесты

---
+++ Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetData.cs
@@ -0,0 +1,30 @@
+﻿using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using System;
+using System.Collections.Generic;
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners;
+
+public class SetData
+{
+    public Partner BasePartner()
+    {
+        var partner = new Partner()
+        {
+            Id = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8"),
+            Name = "Суперигрушки",
+            IsActive = true,
+            PartnerLimits = new List<PartnerPromoCodeLimit>()
+            {
+                new PartnerPromoCodeLimit()
+                {
+                    Id = Guid.Parse("e00633a5-978a-420e-a7d6-3e1dab116393"),
+                    CreateDate = new DateTime(2020, 07, 9),
+                    EndDate = new DateTime(2020, 10, 9),
+                    Limit = 100
+                }
+            }
+        };
+
+        return partner;
+    }
+}
